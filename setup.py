018c375bc4adbad9c4f0ceb05523740c4a46c518
Gaha <med4abdellahi@gmail.com>
2024-05-08 19:31:23 +0200

    setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages,setup
+
+setup(
+    name='ChurnPrj',
+    version='0.0.1',
+    author='Mohamed Abdellahi Lemrabott',
+    author_email='med4abdellahi@gmail.com',
+    packages=find_packages(),
+    install_requires=['pandas','numpy','seaborn' ]
+
+)
